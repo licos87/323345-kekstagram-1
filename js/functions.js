556d9774658d3540e42275e7d710f117ab918cb1
Sergey Ivanov <licos.87@mail.ru>
2024-04-25 09:55:33 +0300

    - переименовывает - оптимизирует - переименовывает переменые и оптимизирует Полиндром - переименовывает функцию извлечения цифр - исправляет возврат отрицательного числа на положительное

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,46 @@
 // Функции для проверки палиндрома
 
-const getPalindrom = (text) => {
-  let primary = text.replaceAll(' ', ''); // убирает пробелы
-  primary = primary.toLowerCase(); // приводит весь текст к маленьким буквам
-  let second = '';
+const isPalindrom = (text) => {
+  let tempText = text.replaceAll(' ', ''); // убирает пробелы
+  tempText = tempText.toLowerCase(); // приводит весь текст к одному регистру букв !!! можно было записать как tempText = text.replaceAll(' ', '').toLowerCase();
+  let reverseText = '';
 
-  for (let i = primary.length - 1; i >= 0; i--) { // перебираем текст и записываем сзадом наперед
-    second += primary[i];
+  for (let i = tempText.length - 1; i >= 0; i--) { // перебираем текст и записываем задом наперед
+    reverseText += tempText[i];
   }
 
-  return (primary.replace(' ', '') === second.replace(' ', '')); // проверяет начальный текст и перевернутый
+  return tempText === reverseText; // проверяет начальный текст и перевернутый
 };
 
-getPalindrom('Лёша на полке клопа нашёл ');
+/*
+const isPalindrom = (text) => {
+	const tempText = text
+		.replaceAll(' ', '')
+		.toLowerCase();
+
+	return tempText === tempText.split('').reverse().join('');
+
+	Сокращеное написание.  Возвращает  и сравнивает tempText = метод переводит tempText в массив ( tempText.split('') ) метод переворачивает массив ( .reverse() ) метод обьеденяет массив в строку ( .join() )
+*/
+
+isPalindrom('Лёша на полке клопа нашёл ');
 
 // Функция извлечения цифр
 
-const getNumber = (string) => {
+const extractNumber = (string) => {
   if (isNaN(string)) { // проверяем является числом, возвращает true если не число.
-    const inNumber = string.match(/\d+/g).join(''); //  равнозначно, что string.match(/[0-9]/g) !учить регулярные выражения. флаг /g показывает что надо перебрать все выражение.  .join('') склеивает все цифры в одно выражение
+    string += '';
+    const inNumber = string.match(/\d+/g).join(''); //  выбирает все цифры, равнозначно, что string.match(/[0-9]/g) !учить регулярные выражения. флаг /g показывает что надо перебрать все выражение.  .join('') склеивает все цифры в одно выражение
     const outNumber = inNumber ? parseInt(inNumber, 10) : NaN; // parseInt(откудаБерет, форматЧисел(десятичные\шестьнадцатиричные)). Если inNumber=true то parseInt иначе NaN,тернарные операторы учить
     return outNumber;
   } else {
-    return string;
+    return parseInt(string.match(/\d+/g).join(''), 10);
   }
 };
 
-getNumber('1 кефир, 0.5 батона');
+// проверка на число if (typeof string === 'number') {return string}
+
+extractNumber('1 кефир, 0.5 батона');
 
 // Функция формирования адресов файлов
 
@@ -49,6 +63,6 @@ getAdress('q', 4, 'we');
 
 // Функция проверки длины строки
 
-const stringLength = (string, number) => (string.length <= number);
+const isStringLength = (string, length) => string.length <= length; // т.к проверка проходит в 1 строку  true||false, стрелочная функция сокращается и убираются все скобки и if
 
-stringLength('проверяемая строка', 10);
+isStringLength('проверяемая строка', 10);
