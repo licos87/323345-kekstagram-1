0fa04f722091d55d8865a8b64c23330126b4c937
Sergey Ivanov <licos.87@mail.ru>
2024-08-15 21:03:22 +0300

    fix: исправляет ошибки линтера ( нейминг )

--- js/images.js
+++ js/images.js
@@ -33,10 +33,10 @@ const dataImg = (({id, url, description, likes, comments}) => {
  * @param { similarImages } Получает на входе симулированные изображения
  * @returns  { [] } Возвращает массив данных на основе similarImg
  */
-const renderLibrary = (images) => {
+const renderLibrary = (pictures) => {
   // Обрабатывает каждое изображение и добавляет результат в массив библотеки.
-  images.forEach((image) => {
-    libraryThumbnail.push(dataImg(image));
+  pictures.forEach((picture) => {
+    libraryThumbnail.push(dataImg(picture));
   });
 
   return libraryThumbnail;
